3f955ffc11f4137afec657a56c6f78d4964b8524
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-05-10 18:21:30 -0300

    updated task 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import App from '../App';
 import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
 describe('Test for Pokédex <FavoritePokemons /> component', () => {
   it('Verifies if Favorite webpage renders a message when no favorite pokemon selected',
