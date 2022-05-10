a5714e2c6def2ab1dde5459b8b8ebcf19fd8ac06
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-05-10 16:26:40 -0300

    Task 3 of project, once more

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import NotFound from '../components/NotFound';
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import NotFound from '../components/NotFound';
 
-describe('Test for Pokédex <NotFound /> component', () => {
-  it('Verifies if',
-    () => {
-});
+// describe('Test for Pokédex <NotFound /> component', () => {
+//   it('Verifies if',
+//     () => {
+// });
