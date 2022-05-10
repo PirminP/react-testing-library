64d2681d35b1371416c17040a24b74fd0df6af7f
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-05-10 13:21:24 -0300

    Add task 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Test for Pokédex <About /> component', () => {
+  it('Verifies if About webpage contains heading with text "About Pokédex"', () => {
+    render(<About />);
+    const subtitleAbout = screen
+      .getByRole('heading', { level: 2, name: /about pokédex/i });
+
+    expect(subtitleAbout).toBeInTheDocument();
+  });
+
+  it('Verifies if About webpage contains two paragraphs with information about Pokédex ',
+    () => {
+      render(<About />);
+      const firstParagraph = screen.getByText(/containing all/);
+      const secondParagraph = screen.getByText(/for each one/);
+
+      expect(firstParagraph).toBeInTheDocument();
+      expect(secondParagraph).toBeInTheDocument();
+    });
+
+  it('Verifies if About webpage contains an image of a Pokédex ', () => {
+    render(<About />);
+    const imagePokedex = screen.getByRole('img', { name: /Pokédex/i });
+
+    expect(imagePokedex).toBeInTheDocument();
+    expect(imagePokedex.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
