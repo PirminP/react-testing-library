256fbd1c195eaa71067f0f64008ddc49ef96da9a
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-05-10 18:07:28 -0300

    Task 4 of project done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,8 +1,29 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import NotFound from '../components/NotFound';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
 
-// describe('Test for Pokédex <NotFound /> component', () => {
-//   it('Verifies if',
-//     () => {
-// });
+describe('Test for Pokédex <NotFound /> component', () => {
+  it('Verifies if NotFound webpage contains heading with text "Page requested not found"',
+    () => {
+      render(<NotFound />);
+
+      const pageNotFound = screen
+        .getByRole('heading', { level: 2, name: /page requested not found/i });
+      const cryingEmoji = screen.getByRole('img', { name: /crying emoji/i });
+
+      expect(pageNotFound).toBeInTheDocument();
+      expect(cryingEmoji).toBeInTheDocument();
+    });
+
+  it('Verifies if NotFound webpage contains image of crying Pikachu',
+    () => {
+      render(<NotFound />);
+
+      const imagePikachu = screen
+        .getByRole('img',
+          { name: /Pikachu crying because the page requested was not found/i });
+
+      expect(imagePikachu).toBeInTheDocument();
+      expect(imagePikachu.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+});
