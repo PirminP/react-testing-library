edb3d9d2e9d323f62a55f57c009564f28a84267f
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-05-10 15:50:38 -0300

    Task 3 ready to go

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test for Pokédex <FavoritePokemons /> component', () => {
+  it('Verifies if Favorite webpage renders a message when no favorite pokemon selected',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      const linkToFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+      userEvent.click(linkToFavorite);
+      expect(history.location.pathname).toBe('/favorites');
+
+      const PokemonNotFound = screen.getByText(/No favorite pokemon found/);
+      expect(PokemonNotFound).toBeInTheDocument();
+    });
+
+  it('Verifies if favorite pokemon is selected and rendered on Favorite webpage',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      const pokemonPikachu = screen.getByText(/pikachu/i);
+      expect(pokemonPikachu).toBeInTheDocument();
+
+      const linkToDetails = screen.getByRole('link', { name: /more details/i });
+      expect(linkToDetails).toBeInTheDocument();
+      userEvent.click(linkToDetails);
+      expect(history.location.pathname).toBe('/pokemons/25');
+
+      const favoriteSelected = screen.getByRole('checkbox');
+      expect(favoriteSelected).toBeInTheDocument();
+      userEvent.click(favoriteSelected);
+
+      const linkToFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+      userEvent.click(linkToFavorite);
+      expect(history.location.pathname).toBe('/favorites');
+
+      const pikachuAsFavorite = screen.getByText(/pikachu/i);
+      expect(pikachuAsFavorite).toBeInTheDocument();
+    });
+});
